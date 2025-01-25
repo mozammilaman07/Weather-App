e6b5a5b2d6cee43166cf31f407ad1a8698cb5b94
Md Mozammil Aman <amanmozammil.07@gmail.com>
2025-01-25 23:15:54 +0530

    all files and folder added

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import SearchBox from "./SearchBox";
+import InfoBox from "./InfoBox";
+import WeatherApp from "./WeatherApp";
+
+function App() {
+  return (
+    <div className="App">
+      <WeatherApp />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/InfoBox.jsx
@@ -0,0 +1,62 @@
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import "./InfoBox.css";
+import ThunderstormIcon from "@mui/icons-material/Thunderstorm";
+import AcUnitIcon from "@mui/icons-material/AcUnit";
+import WbSunnyIcon from "@mui/icons-material/WbSunny";
+
+export default function InfoBox({ info }) {
+  const INIT_URL =
+    "https://media.istockphoto.com/id/157527872/photo/storm-cloud.webp?a=1&b=1&s=612x612&w=0&k=20&c=NQtwRlo4lqq0rC9o8Cr24CqGN2LR_L2QcE2k3elrbf8=";
+
+  const HOT_URL =
+    "https://images.unsplash.com/photo-1504370805625-d32c54b16100?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8aG90JTIwd2VhdGhlcnxlbnwwfHwwfHx8MA%3D%3D";
+  const COLD_URL =
+    "https://plus.unsplash.com/premium_photo-1670604649107-a0171e5f1bd0?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NXx8Y29sZCUyMHdlYXRoZXJ8ZW58MHx8MHx8fDA%3D";
+  const RAIN_URL =
+    "https://images.unsplash.com/photo-1475116127127-e3ce09ee84e1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTh8fHJhaW4lMjB3ZWF0aGVyfGVufDB8fDB8fHww";
+
+  return (
+    <div className="Info">
+      <div className="CardConatiner">
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            sx={{ height: 140 }}
+            image={
+              info.humidity > 80
+                ? RAIN_URL
+                : info.temp > 15
+                ? HOT_URL
+                : COLD_URL
+            }
+            title="green iguana"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {info.city}{" "}
+              {info.humidity > 80 ? (
+                <ThunderstormIcon />
+              ) : info.temp > 15 ? (
+                <WbSunnyIcon />
+              ) : (
+                <AcUnitIcon />
+              )}
+            </Typography>
+            <Typography variant="body2" sx={{ color: "text.secondary" }}>
+              <p>Temperature = {info.temp}&deg;C</p>
+              <p>Humidity = {info.humidity}</p>
+              <p>Min Temp = {info.tempMin}&deg;C</p>
+              <p>Max Temp = {info.tempMax}&deg;C</p>
+              <p>
+                The Weather can be described as <i>{info.weather}</i> and feels
+                like {info.feelsLike}&deg;C
+              </p>
+            </Typography>
+          </CardContent>
+        </Card>
+      </div>
+    </div>
+  );
+}
